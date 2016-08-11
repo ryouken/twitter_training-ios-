c972bb3cf872eb169f6af87c1d13c6e4f8c8c969
小島領剣 <ryouken@mbp-by-ryouken-kojima.local>
2016-08-11 17:43:40 +0900

    Initial Commit

---
+++ twitter_training/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter_training
+//
+//  Created by 小島領剣 on 2016/08/11.
+//  Copyright © 2016年 Ryouken Kojima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
